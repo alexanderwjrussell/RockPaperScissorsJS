65cd0e36a7c4ff59a5c3a130e73f609ded6ec4c2
Alexander Russell <awjrussell@live.co.uk>
2017-10-19 13:05:27 +0100

    Player 1) Added ability to track games played, games won and games lost

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -2,17 +2,22 @@ var Player = require('../src/player.js')
 
 describe("Player", function() {
 
+    var player;
+    
+    beforeEach(function() {
+        player = new Player();
+    });
+
     describe("Initialisation", function() {
 
         it("should have a default name property", function() {
-            var player = new Player();
             expect(player.name).toBe("Default");
         });
 
         it("should allow the user to choose a name on creation", function() {
             var name = "Test";
-            var player = new Player(name);
-            expect(player.name).toBe("Test");
+            var player1 = new Player(name);
+            expect(player1.name).toBe("Test");
         });
 
         it("should be able to handle multiple players and maintain their name state", function() {
@@ -23,15 +28,51 @@ describe("Player", function() {
             expect(player1.name).toBe("Player 1");
             expect(player2.name).toBe("Player 2")
         });
+
+        it("should have 0 games played on creation", function() {
+            expect(player.gamesPlayed).toBe(0);
+        });
+
+        it("should have 0 games won on creation", function() {
+            expect(player.gamesWon).toBe(0);
+        });
+
+        it("should have 0 games lost on creation", function() {
+            expect(player.gamesLost).toBe(0);
+        });
     });
 
     describe("Post created properties", function() {
 
         it("should allow you to update your name using the updateName mathod", function() {
-            var player = new Player();
             var name = "Updated name";
             player.updateName(name);
             expect(player.name).toBe("Updated name");
+        });
+
+        it("should increase gamesPlayed when a game is played", function() {
+            player.gamePlayed();
+            expect(player.gamesPlayed).toBe(1);
+        });
+
+        it("should allow a player to increase games won using the gameWon method", function() {
+            player.gameWon();
+            expect(player.gamesWon).toBe(1);
+        });
+
+        it("should allow a player to increase games lost using the gameLost method", function() {
+            player.gameLost();
+            expect(player.gamesLost).toBe(1);
+        });
+
+        it("should increment gamesPlayed when a game is won", function() {
+            player.gameWon();
+            expect(player.gamesPlayed).toBe(1);
+        });
+
+        it("should increment gamesPlayed when a game is lost", function() {
+            player.gameLost();
+            expect(player.gamesPlayed).toBe(1);
         });
     });
 });
--- src/player.js
+++ src/player.js
@@ -2,10 +2,27 @@
 
 function Player(name = "Default") {
     this.name = name;
+    this.gamesPlayed = 0;
+    this.gamesWon = 0;
+    this.gamesLost = 0;
 }
 
 Player.prototype.updateName = function(name) {
     this.name = name;
+}
+
+Player.prototype.gamePlayed = function() {
+    this.gamesPlayed += 1;
+}
+
+Player.prototype.gameWon = function() {
+    this.gamePlayed();
+    this.gamesWon += 1;
+}
+
+Player.prototype.gameLost = function() {
+    this.gamePlayed();
+    this.gamesLost += 1;
 }
 
 module.exports = Player;
